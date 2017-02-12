27d6c96ae48504a4cf10e4fabc39331447174d9c
agoddijn <alex.goddijn@gmail.com>
2017-02-12 14:45:48 -0800

    Rewrote mobility (again)

---
+++ data/blogs/ai.js
@@ -0,0 +1,11 @@
+var path = "/content/images/blogs/";
+
+module.exports = {
+  id: 2,
+  title: "AI",
+  image: path + "hal.png",
+  heightDesktop: "700",
+  heightMobile: "1300",
+  date: new Date(2017, 1, 11), //yyyy, mm+1, dd
+  content: ""
+}
