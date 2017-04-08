b2a3894616aad7599c0fdf2ee71062f24e0097b3
agoddijn <alex.goddijn@gmail.com>
2017-04-07 21:37:00 -0700

    Added ubcInsight

---
+++ data/projects/ubcInsight.js
@@ -0,0 +1,20 @@
+var path = "/content/images/projects/ubcInsight/";
+
+module.exports = {
+  title: "UBC Insight",
+  icon: "/content/images/logos/ubcInsightLogo.png",
+  github: "https://github.com/agoddijn/cs310Project",
+  descriptionShort: "Queryable database project with GUI",
+  sections: [
+    {
+      header: "Introduction",
+      descriptionAbove: "This was a project course at UBC, CPSC 310, where the goal was to create a queryable database with frontend GUI for a dataset of rooms and courses offered at UBC. We had to implement this in Typescript with node as a backend framework. ",
+      descriptionBelow: "",
+      additional: [
+        {
+          type: ""
+        }
+      ]
+    }
+  ]
+}
