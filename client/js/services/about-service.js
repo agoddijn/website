8608a867f97f2da3b18ad846f9df4476066a3156
agoddijn <alex.goddijn@gmail.com>
2017-08-10 19:59:22 +0200

    Rewording and favicon buttons

--- client/js/services/about-service.js
+++ client/js/services/about-service.js
@@ -2,17 +2,17 @@ app.service('AboutData', function(){
   var shortDescription = "I'm Alexander Goddijn, a French and Dutch national passionate about product and AI, currently a Computer Science and Physics student at UBC";
 
   var longDescription = [
-    "I was born in Amsterdam, where I spent the first four years of my life before moving to London. I learnt english and spent most of my formative academic years there. During these years I began to develop my interests, fiction and physics being the predominant ones. I either had my nose in a book, or was pointing things out to people trying to explain how they worked.",
-    "These interests developed into passions and at the age of 13, we moved back to Amsterdam, where I finished my high school education in the IB system, focusing on sciences and mathematics. It was during high school that I also developed an interest in coding, and my intructional YouTube videos can be seen in the projects tab of this website.",
-    "I decided to leave Europe and move to Canada, chiefly for its proximity to the best ski slopes and mountain biking trails in the world. I have not regretted my decision ;). Here I moved through a myriad of different studies, starting in combined sciences, then moving towards engineering and physics, and finally settling into physics and computer science.",
-    "My focus in these areas are AI and machine learning. I have always been fascinated by the phenomenon of consciousness, and through understanding AI and machine learning algorithms I hope to come closer to understanding the human experience. After much trying and searching, I think I am willing to declare this as my main focus in the future, and hope to base my career on it.",
+    "I was born in Amsterdam, where I spent the first four years of my life before moving to London. I learnt english and spent most of my formative academic years there. During these years I began to develop an interest in fiction and physics. I either had my nose in a book, or was pointing things out to people trying to explain how they worked.",
+    "These interests continued to develop and I decided to focus my IB studies on physics and mathematics. It was during this time that I also discovered an interest in coding, and my intructional YouTube videos can be seen in the projects tab of this website. What attracted me to this field was the ability to produce something meaningful that others could enjoy using. I still love creating programs that are easy and intuitive for my users",
+    "After finishing the IB in Amsterdam, I decided to leave Europe and move to Canada, chiefly for its proximity to the best ski slopes and mountain biking trails in the world. Here I moved through a myriad of different studies, starting in combined sciences, then moving towards engineering and physics, and finally settling into physics and computer science. During all of these stages I enrolled in specialized programs, firstly in Science One, then in Engineering Physics, and towards the end in the New Venture Design Program. All of these programs had a separate interview process and were much more challenging than the ordinary curriculum.",
+    "My main interests in computer science are AI and machine learning. I have always been fascinated by the phenomenon of consciousness, and through understanding AI and machine learning algorithms I hope to come closer to understanding the human experience.",
   ]
 
   var info = [
     "Fluent in three languages (Dutch, French, English)",
     "Studying physics, engineering, and computer science at the University of British Columbia in Vancouver",
     "Sports - skiing, mountain biking, sailing, climbing, water polo, running",
-    "Hobbies - reading, film, music"
+    "Hobbies - reading, film, music, podcasts, documentaries, wikipedia"
   ];
 
   // Pics should have 0.665 aspect ratio
