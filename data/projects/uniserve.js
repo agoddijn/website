1397f36a6d98792021fd82a213a32544fb089b63
agoddijn <alex.goddijn@gmail.com>
2018-03-08 20:25:32 -0800

    finished uniserve project

--- data/projects/uniserve.js
+++ data/projects/uniserve.js
@@ -13,14 +13,14 @@ module.exports = {
     sections: [
         {
             header: "Introduction",
-            descriptionAbove: "",
+            descriptionAbove: "As part of a university project, we were given the task of building a web app to meet the specifications of a client, Uniserve. They wanted an analytics app that displayed the health of their customers servers. We were required to routinely ping all the devices on their list, log their response times, store them, and create an app that would insight into how their devices were doing.",
             descriptionBelow: "",
             additional: []
         },
         {
             header: "Product",
-            descriptionAbove: "",
-            descriptionBelow: "",
+            descriptionAbove: "We ended with a nice, interactive, modular web app, where users can see a list of their devices, their status, a chart with displaying the health of the devices over a select period of time, and a map with the location and status of their servers.",
+            descriptionBelow: "On the backend, we created a series of node apps that simultaneously. One, the Pinger, would ping devices, log information, and send this to the database. It includes a failsafe, so if the database is not responding, the information is not lost. The second aggregates the older ping records, so the database doesn't exceed capacity. The third is a node web backend that provides an api for accessing and updating the database.",
             additional: [
                 {
                     type: "image",
@@ -61,8 +61,8 @@ module.exports = {
         },
         {
             header: "Design",
-            descriptionAbove: "",
-            descriptionBelow: "",
+            descriptionAbove: "We had members on our team that were experienced in setting up a scalable development environment. This included automated builds, deployment scripts, unit testing, performance testing, and a host of database tools to flood the development environments with fake test data.",
+            descriptionBelow: "The scalable design allowed us to work very efficiently, and make sure the codebase was easy to change once it was handed off to the customer. It was also well documented, in order to ensure a smooth handoff from us to uniserve.",
             additional: [
                 {
                     type: "code",
@@ -83,8 +83,8 @@ module.exports = {
         },
         {
             header: "Tools",
-            descriptionAbove: "",
-            descriptionBelow: "",
+            descriptionAbove: "We used an array of modern tools to develop the app. A lot of the tools were used for automation of the workflow, but also to ensure the app could be seamlessly worked on by multiple people.",
+            descriptionBelow: "Typescript ended up being very useful for development. It made me realise how important type checking is for large projects that need to move quickly.",
             additional: [
                 {
                     type: "image",
